10e125be3096ee61f276db4ce7a610b9d08ec403
chris-dem <christosdeme@gmail.com>
2026-01-27 23:11:36 +0200

    Full send

--- lib/Lib/CircuitType.hs
+++ lib/Lib/CircuitType.hs
@@ -10,7 +10,9 @@
 
 module Lib.CircuitType where
 
+import Data.Foldable1 qualified as S
 import Data.Kind (Type)
+import Data.Sequence qualified as S
 import GHC.TypeNats
 import Lib.HList
 
@@ -31,15 +33,35 @@ instance BooleanFunction FormulaFunc where
 
 data CircuitFunc (k :: Nat) (n :: Nat) where
     EmptyCircuit :: CircuitFunc 0 n
-    Input :: (i + 1 <= n) => VSNat i -> CircuitFunc k n -> CircuitFunc (k + 1) n
-    AndStatement :: ((1 <= i), (1 <= j), (i + 1 <= k), (j + 1 <= k)) => VSNat i -> VSNat j -> CircuitFunc k n -> CircuitFunc (k + 1) n
-    OrStatement :: ((1 <= i), (1 <= j), (i + 1 <= k), (j + 1 <= k)) => VSNat i -> VSNat j -> CircuitFunc k n -> CircuitFunc (k + 1) n
-    NotStatement :: (i + 1 <= k, 1 <= i) => VSNat i -> CircuitFunc k n -> CircuitFunc (k + 1) n
+    Input :: (i + 1 <= n) => VSNat i -> CircuitFunc (k - 1) n -> CircuitFunc k n
+    AndStatement :: ((1 <= i), (1 <= j), (i <= k - 1), (j <= k - 1)) => VSNat i -> VSNat j -> CircuitFunc (k - 1) n -> CircuitFunc k n
+    OrStatement :: ((1 <= i), (1 <= j), (i <= k - 1), (j <= k - 1)) => VSNat i -> VSNat j -> CircuitFunc (k - 1) n -> CircuitFunc k n
+    NotStatement :: (i <= k - 1, 1 <= i) => VSNat i -> CircuitFunc (k - 1) n -> CircuitFunc k n
 
 newtype BoolCircuit (n :: Nat) = BoolCircuit (CircuitFunc (n + 1) n)
 
 instance BooleanFunction (CircuitFunc k) where
-    apply s b = evalCirc s b ()
+    apply s b = final
+      where
+        (a S.:|> final) = evalCirc s b S.empty
 
-
-evalCirc = undefined
+evalCirc :: CircuitFunc k n -> Vec n Bool -> S.Seq Bool -> S.Seq Bool
+evalCirc EmptyCircuit _ v = v
+evalCirc (Input s r) b v = evalCirc r b v S.:|> indexVS s b
+evalCirc (AndStatement firstInd secInd rest) b v = m S.:|> result
+  where
+    m = evalCirc rest b v
+    i1 = toVSInt firstInd
+    i2 = toVSInt secInd
+    result = m `S.index` i1 && m `S.index` i2
+evalCirc (OrStatement firstInd secInd rest) b v = m S.:|> result
+  where
+    m = evalCirc rest b v
+    i1 = toVSInt firstInd
+    i2 = toVSInt secInd
+    result = m `S.index` i1 || m `S.index` i2
+evalCirc (NotStatement firstInd rest) b v = m S.:|> result
+  where
+    m = evalCirc rest b v
+    i1 = toVSInt firstInd
+    result = not $ m `S.index` i1
